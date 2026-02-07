fa6bd2e797fa62d8b901800bcf6ef93cc4b6aeef
Casey-Bird <caseybird22@outlook.com>
2026-02-06 22:50:17 -0500

    Initial axum setup

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod util;
+
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+pub async fn main() {
+
+    let app: Router = create_app();
+
+    let listener: TcpListener = TcpListener::bind("0.0.0.0:3080").await.expect("Failed to bind tcp listener");
+
+    axum::serve(listener, app)
+    .await
+    .expect("Failed to start server");
+
+}
+
+fn create_app() -> Router {
+
+    Router::new()
+    .route("/", get(|| async { "Hello, world!" }))
+
+
+
+}
+
+
---
+++ src/util/logger.rs
@@ -0,0 +1,21 @@
+
+
+struct Logger;
+
+
+
+impl Logger {
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
